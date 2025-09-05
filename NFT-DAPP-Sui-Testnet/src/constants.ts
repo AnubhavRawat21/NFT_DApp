045e51d4677d1b85b5f294a21739d274ab4c2d61
Anubhav Rawat <anubhav2021rawat@gmail.com>
2025-09-05 17:31:38 +0530

    Packageid Changed

--- NFT-DAPP-Sui-Testnet/src/constants.ts
+++ NFT-DAPP-Sui-Testnet/src/constants.ts
@@ -5,5 +5,5 @@
 
 // NFT Package IDs
 export const DEVNET_NFT_PACKAGE_ID = "0xTODO"; // Replace with your devnet package ID
-export const TESTNET_NFT_PACKAGE_ID = "0xbe47df4fbec1d79b42bee2c42315b376f1c6401da77becf250ee4eed97d2f5c9";
+export const TESTNET_NFT_PACKAGE_ID = "0xf6df7f9550a1424d32d1db5fc7a1643fede1437bab030b18af41b4940f738665";
 export const MAINNET_NFT_PACKAGE_ID = "0xTODO"; // Replace with your mainnet package ID
